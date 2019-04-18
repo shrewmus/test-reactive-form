63a52e3fd22ed4ecef8566f0ac9f88f3855653f3
Artem Goriachev <shrewmus@gmail.com>
2019-04-18 23:53:43 +0200

    simple example with generated fields via formarray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'reacitve-form-test';
+  myForm: FormGroup;
+
+  get rules() {
+    return this.myForm.get('rules') as FormArray;
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      rules: this.formBuilder.array([])
+    })
+
+  }
+
+  addRule() {
+    this.rules.push(this.formBuilder.group({
+      customUrl:['', {validators: [Validators.required], updateOn:'change'}]
+    }));
+  }
+
+  removeRule(i) {
+    this.rules.removeAt(i);
+  }
 }
